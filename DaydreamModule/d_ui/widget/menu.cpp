991d0bb3050a65e3acbc8d30516cbdd1ecb07a3d
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-06-18 20:24:12 +0800

    Add empty widget file

---
+++ DaydreamModule/d_ui/widget/menu.cpp
@@ -0,0 +1,63 @@
+#include "menu.hpp"
+
+namespace daydream {
+namespace ui {
+
+	sub_menu::sub_menu(const std::string& name) :m_name(name) {
+	}
+
+	void sub_menu::impl_imgui_render() {
+		if (ImGui::BeginMenu(m_name.c_str()))
+		{
+			for (auto& item : m_actions) {
+				if (ImGui::MenuItem(item.m_name.c_str(), item.m_accelerate_key.c_str())) {
+					emit item.clicked();
+				}
+			}
+			ImGui::EndMenu();
+		}
+	}
+
+	sub_menu& menu::get_sub_menu(size_t idx) {
+		if (idx > m_sub_menus.size()) {
+			std::cout << "[Err]: Indexing sub menu from main menu, error, out of range.";
+			std::cout << "Find size() is " << m_sub_menus.size() << ". But indexing size is " << idx << ".\n";
+			exit(1);
+		}
+
+		return m_sub_menus[idx];
+	}
+
+	void menu::add_sub_menu(sub_menu& rhs) {
+		m_sub_menus.push_back(rhs);
+	}
+
+	void menu::add_sub_menu(size_t idx, sub_menu& rhs) {
+		m_sub_menus.insert(m_sub_menus.begin() + idx, rhs);
+	}
+
+	void menu::remove_sub_menu(sub_menu& rhs) {
+		for (auto it = m_sub_menus.begin(); it != m_sub_menus.end(); ) {
+			if (it->name() == rhs.name()) {
+				m_sub_menus.erase(it);
+			}
+			++ it;
+		}
+	}
+
+	void menu::impl_imgui_render() {
+		if (ImGui::BeginMenuBar())
+		{
+			for (auto& item : m_sub_menus) {
+				item.impl_imgui_render();
+			}
+			ImGui::EndMenuBar();
+		}
+	}
+
+	void action::clicked() {
+
+	}
+
+} //! namesapce daydream
+} //! namespace daydream
---
+++ DaydreamModule/d_ui/widget/menu.hpp
@@ -0,0 +1,58 @@
+#ifndef H_UI_WIDGET_MENU
+#define H_UI_WIDGET_MENU
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "_base.hpp"
+
+namespace daydream {
+namespace ui {
+
+	class D_API_EXPORT action {
+	public:
+		std::string		m_name;
+		std::string		m_accelerate_key;
+
+	public:
+		void clicked();
+	};
+
+	class D_API_EXPORT sub_menu {
+	public:
+		sub_menu(const std::string &name);
+
+		const std::string& name() const { return m_name; }
+	
+	public:
+		virtual void update() {};
+		virtual void impl_imgui_render();
+
+	private:
+		std::string				m_name;
+		std::vector<action>		m_actions;
+	};
+
+	class D_API_EXPORT menu {
+	public:
+		menu() {};
+		~menu() {};
+
+		void			add_sub_menu(sub_menu &rhs);
+		void			add_sub_menu(size_t idx, sub_menu& rhs);
+		void			remove_sub_menu(sub_menu& rhs);
+		sub_menu&		get_sub_menu(size_t idx);
+
+	public:
+		virtual void	update() {};
+		virtual void	impl_imgui_render();
+
+	private:
+		std::vector<sub_menu>	m_sub_menus;
+	};
+
+} //! namespace ui
+} //! namespace daydream
+
+#endif // !H_UI_WIDGET_MENU
