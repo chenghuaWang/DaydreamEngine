718f2859a940a90a5a0af2c0b8c8f46c3fab2aaa
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-06-18 20:22:31 +0800

    Daydream driver func for testing

--- DaydreamEngine/DaydreamEngine.cpp
+++ DaydreamEngine/DaydreamEngine.cpp
@@ -2,18 +2,10 @@
 //
 
 #include "DaydreamEngine.h"
-#include "d_ui/window/window.hpp"
 
 using namespace std;
-using namespace daydream;
-
-class main_window: public ui::base_window {
-public:
-	main_window(size_t w, size_t h, const std::string& name) : ui::base_window(w, h, name) {}
-
-};
 
 int main() {
-	main_window app_window(1920, 1080, "DreamMaker");
+	ui::imgui_window	app_window;
 	app_window.exec();
 }
