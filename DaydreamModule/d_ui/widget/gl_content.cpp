707702212b57ef844a52a7ea9655f0222ff6e2fb
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-09-02 14:06:57 +0800

    fix window focus bug

--- DaydreamModule/d_ui/widget/gl_content.cpp
+++ DaydreamModule/d_ui/widget/gl_content.cpp
@@ -68,13 +68,15 @@ void gl_content::on_detach() {
 void gl_content::on_update() {
   m_timer.OnUpdate();
   if (m_running) {
-    if (ImGui::IsWindowFocused) {
-      if (ImGui::IsMouseClicked) {
+    if (ImGui::IsWindowFocused()) {
+      if (ImGui::IsMouseClicked(0) || ImGui::IsMouseClicked(1) || ImGui::IsMouseClicked(2)) {
         m_cur_mouse_click.x = ImGui::GetMousePos().x - ImGui::GetWindowPos().x;
         m_cur_mouse_click.y = ImGui::GetMousePos().y - ImGui::GetWindowPos().y;
+        if (ImGui::IsMouseClicked(0) || ImGui::IsMouseClicked(1)) {
+          m_cur_mouse_pos.x = ImGui::GetMousePos().x - ImGui::GetWindowPos().x;
+          m_cur_mouse_pos.y = ImGui::GetMousePos().y - ImGui::GetWindowPos().y;
+        }
       }
-      m_cur_mouse_pos.x = ImGui::GetMousePos().x - ImGui::GetWindowPos().x;
-      m_cur_mouse_pos.y = ImGui::GetMousePos().y - ImGui::GetWindowPos().y;
     }
   }
   if (m_running) {
